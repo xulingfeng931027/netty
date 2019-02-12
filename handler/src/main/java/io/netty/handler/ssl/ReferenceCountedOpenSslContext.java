6fbc5748e1ed0d9a8e5cf7e33979dc1b871d4c8e
Norman Maurer <norman_maurer@apple.com>
2019-02-12 10:25:48 -0800

    Use system property to enable / disable usage of tasks

--- handler/src/main/java/io/netty/handler/ssl/ReferenceCountedOpenSslContext.java
+++ handler/src/main/java/io/netty/handler/ssl/ReferenceCountedOpenSslContext.java
@@ -86,7 +86,8 @@ public abstract class ReferenceCountedOpenSslContext extends SslContext implemen
                                                       2048));
                 }
             });
-
+    private static final boolean USE_TASKS =
+            SystemPropertyUtil.getBoolean("io.netty.handler.ssl.openssl.useTasks", false);
     private static final Integer DH_KEY_LENGTH;
     private static final ResourceLeakDetector<ReferenceCountedOpenSslContext> leakDetector =
             ResourceLeakDetectorFactory.instance().newResourceLeakDetector(ReferenceCountedOpenSslContext.class);
@@ -344,7 +345,7 @@ public abstract class ReferenceCountedOpenSslContext extends SslContext implemen
             }
 
             // TODO: Make the configurable.
-            SSLContext.setUseTasks(ctx, true);
+            SSLContext.setUseTasks(ctx, USE_TASKS);
             success = true;
         } finally {
             if (!success) {
